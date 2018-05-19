121147ad9476b300e5f572701d589c3f74e8b68e
shadowzack <battleforge-war@hotmail.com>
2018-05-20 00:20:54 +0300

    new

--- server.js
+++
@@ -1,8 +0,0 @@
-const http =require('http');
-const port = process.env.PORT || 3000;
-const app= require('./app');
-const server = http.createServer(app);
-
-server.listen(port,()=>{
-    console.log('server is running on port:'+ port);
-});
